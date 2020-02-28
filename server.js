a08aba5ae5478488b956d4cab355bd597306c450
Bibek Shrestha <shresthabbks@gmail.com>
2020-02-28 11:06:21 +0545

    hellow

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ mongoose
   });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
+  // app.use(express.static("client/build"));
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
   });
 }
