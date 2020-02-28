c89432d9166e322f6b851dc6f40c9946b541751b
Bibek Shrestha <shresthabbks@gmail.com>
2020-02-28 20:18:49 +0545

    suceeeeess

--- app.js
+++ app.js
@@ -13,17 +13,10 @@ const cors = require("cors");
 const GlobalAppError = require("./controller/globalErr");
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 // app.use(express.static("client/build"));
 // app.use(bodyParser.urlencoded({ extended: true }));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.all((req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
-  });
-}
 
 app.use("/Home", Home);
 app.use("/Article", Article);
@@ -36,6 +29,12 @@ app.all("*", (req, res, next) => {
   (err.status = "true"), (err.statusCode = 404);
   next(new AppErr("page not found", 400));
 });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.all((req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
+  });
+}
 
 app.use(GlobalAppError);
 
