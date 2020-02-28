8b12e9113bed6eccd9aa72b080966815d1428d75
Bibek Shrestha <shresthabbks@gmail.com>
2020-02-28 09:52:12 +0545

    suexx

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose
   });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
