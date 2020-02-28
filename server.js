85dc7f3579f9d9e2bd7c3f05eb142b4644c38a92
Bibek Shrestha <shresthabbks@gmail.com>
2020-02-28 09:55:30 +0545

    sucess

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ mongoose
     console.log("Internal Server Error", e);
   });
 
-if (process.env.NODE_ENV === "production") {
-  // app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   // app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
+//   });
+// }
 const port = process.env.PORT || 9000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
